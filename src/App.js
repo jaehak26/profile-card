e91bf23223a4249e884858077618961a16232048
jaehak26 <68010205+jaehak26@users.noreply.github.com>
2022-12-16 23:40:32 +0900

    프로필 로컬에 다운로드 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './style.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import Icon from '@mui/material/Icon';
@@ -9,12 +9,14 @@ import InputUserInfo from './InputUserInfo';
 import { useRecoilState } from 'recoil';
 import { inputBase64 } from './recoil/Base64';
 import { UserInfo } from './recoil/UserInfo';
+import { onClickDownload } from './OnClick';
 
 import { ICON_CSS, PHONE_ICON_CSS } from './style';
 
 export default function App() {
   const [baseDataCoil, setBaseDataCoil] = useRecoilState(inputBase64);
   const [userCoil, setuserCoil] = useRecoilState(UserInfo);
+  const ref = useRef();
 
   useEffect(() => {
     const node = loadCSS(
@@ -29,45 +31,56 @@ export default function App() {
   }, []);
   return (
     <div>
-      <section>
-        <Icon baseClassName="fas" className="fa-bars" />
-        <Icon baseClassName="far" className="fa-sticky-note" />
-        <article class="profile">
-          <br />
-          <div>
-            {baseDataCoil != null && (
-              <img src={`data:image;base64,${baseDataCoil}`} alt="profile" />
-            )}
-          </div>
-          <h1>{userCoil.userName ? userCoil.userName : 'name'}</h1>
-          <h2>
-            {userCoil.moreInfo ? userCoil.moreInfo : 'for more information'}
-          </h2>
+      <p ref={ref}>
+        <section>
+          <Icon baseClassName="fas" className="fa-bars" />
+          <Icon
+            baseClassName="far"
+            className="fa-sticky-note"
+            onClick={() => onClickDownload(ref)}
+          />
+          <article class="profile">
+            <br />
+            <div>
+              {baseDataCoil != null && (
+                <img src={`data:image;base64,${baseDataCoil}`} alt="profile" />
+              )}
+            </div>
+            <h1>{userCoil.userName ? userCoil.userName : 'name'}</h1>
+            <h2>
+              {userCoil.moreInfo ? userCoil.moreInfo : 'for more information'}
+            </h2>
 
-          <a href="#" class="btnView">
-            View More
-          </a>
+            <a href="#" class="btnView">
+              View More
+            </a>
 
-          <ul class="contact">
-            <li>
-              <Icon
-                baseClassName="fas"
-                className="fa-phone"
-                sx={{ ...ICON_CSS }}
-              />
-              <span className="afterIcon">
-                {userCoil.userPhone ? userCoil.userPhone : '010-0000-0000'}
-              </span>
-            </li>
-            <li>
-              <Icon baseClassName="fas" className="fa-envelope" sx={ICON_CSS} />
-              <span className="afterIcon">
-                {userCoil.userEmail ? userCoil.userEmail : 'email@email.com'}
-              </span>
-            </li>
-          </ul>
-        </article>
-      </section>
+            <ul class="contact">
+              <li>
+                <Icon
+                  baseClassName="fas"
+                  className="fa-phone"
+                  sx={{ ...ICON_CSS }}
+                />
+                <span className="afterIcon">
+                  {userCoil.userPhone ? userCoil.userPhone : '010-0000-0000'}
+                </span>
+              </li>
+              <li>
+                <Icon
+                  baseClassName="fas"
+                  className="fa-envelope"
+                  sx={ICON_CSS}
+                />
+                <span className="afterIcon">
+                  {userCoil.userEmail ? userCoil.userEmail : 'email@email.com'}
+                </span>
+              </li>
+            </ul>
+          </article>
+        </section>
+        <br />
+      </p>
       <InputUserInfo></InputUserInfo>
       <ImgUpload></ImgUpload>
     </div>
